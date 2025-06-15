87dec371e63905e838ff3c397a5191e0dee8b435
MilaimKas <milaim.kas@gmail.com>
2025-06-15 21:25:07 +0200

    add rope, summary and docstrings

--- src/PyBayesAB/base_model.py
+++ src/PyBayesAB/base_model.py
@@ -8,6 +8,7 @@ from scipy.stats import gaussian_kde
 class BayesianModel:
 
     def __init__(self):
+
         self.dataA = []
         self.dataB = []
     
@@ -16,15 +17,6 @@ class BayesianModel:
     """
     
     def return_data(self, group):
-        """
-        Returns the data for the specified group.
-
-        Args:
-            group (str): The group to return data for ("A" or "B").
-
-        Returns:
-            list: The data for the specified group.
-        """
         if group == "A":
             return self.dataA
         elif group == "B":
@@ -33,134 +25,88 @@ class BayesianModel:
             raise ValueError("Group must be either 'A' or 'B'")
         
     def add_experiment(self, value, group="A"):
-        """
-        Adds an experiment value to the specified group.
-
-        Args:
-            value (float): The value to add.
-            group (str, optional): The group to add the value to ("A" or "B"). Defaults to "A".
-        """
         if group not in ["A", "B"]:
             raise ValueError("Group must be 'A' or 'B'")
         data = self.dataA if group == "A" else self.dataB
         data.append(value)
 
     def make_rvs_diff(self, N_sample=N_SAMPLE):
-        """
-        Generates random value samples from the posterior distribution for both groups and returns their difference.
-
-        Args:
-            N_sample (int, optional): Number of samples to generate. Defaults to N_SAMPLE.
-
-        Returns:
-            np.array: The difference between the samples from group A and group B.
-        """
         rvs_A = self.make_rvs(group="A", N_sample=N_sample)
         rvs_B = self.make_rvs(group="B", N_sample=N_sample)
-        return rvs_A - rvs_B
+        return rvs_A-rvs_B
 
     def prob_best(self):
-        """
-        Calculates the probability that group A is better than group B.
-
-        Returns:
-            float: The probability that group A is better than group B.
-        """
-        rvs = self.make_rvs_diff()
-        return 100 * np.mean(rvs > 0)
-
-    def hdi(self, group="diff", level=95, post_type="rvs"):
-        """
-        Calculates the highest density interval (HDI) for the specified group.
+        """_summary_
 
         Args:
-            group (str, optional): The group to calculate the HDI for ("A", "B", or "diff"). Defaults to "diff".
-            level (int, optional): The confidence level for the HDI. Defaults to 95.
-            post_type (str, optional): The type of posterior distribution ("rvs" or "pdf"). Defaults to "rvs".
+            rvs (_type_): _description_
 
         Returns:
-            tuple: The lower and upper bounds of the HDI.
+            _type_: _description_
         """
-        if group == "diff" and post_type == "pdf":
+        rvs = self.make_rvs_diff()
+        return 100*(np.mean(rvs > 0))
+
+    def hdi(self, group="diff", level=95, post_type="rvs", norm_app=False):
+        """_summary_
+
+        Args:
+            distribution (_type_): _description_
+            level (int, optional): _description_. Defaults to 95.
+        """
+        if group=="diff" and post_type=="pdf":
             print("Warning: need rvs for hdi of difference. Will proceed using rvs.")
-        if group == "diff":
+        if group=="diff":
             post = self.make_rvs_diff()
-        elif post_type == "pdf":
+        elif post_type=="pdf":
             post = self.make_pdf(group=group)
         else:
             post = self.make_rvs(group=group)
-        return helper.hdi(post, level=level / 100)
+        return helper.hdi(post, level=level/100, norm_app=norm_app)
 
     def rope(self, interval, group="diff"):
-        """
-        Calculates the region of practical equivalence (ROPE) for the specified group.
+        """_summary_
 
         Args:
-            interval (list or tuple): The interval for the ROPE.
-            group (str, optional): The group to calculate the ROPE for ("A", "B", or "diff"). Defaults to "diff".
+            rvs (_type_): _description_
+            interval (_type_): _description_
 
         Returns:
-            float: The percentage of the posterior distribution within the ROPE.
+            _type_: _description_
         """
         if group == "diff":
             rvs = self.make_rvs_diff()
         else:
             rvs = self.make_rvs(group=group)
-        return 100 * (1 - np.mean((rvs < np.max(interval)) & (rvs > np.min(interval))))
+        return bayesian_functions.rope(rvs=rvs, interval=interval)*100
 
-    def rope_decision(self, interval, group="diff", level=95):
-        """
-        Makes a decision based on the ROPE and HDI for the specified group.
+    def rope_decision(self, rope_interval, level=95):
+        """_summary_
 
         Args:
-            interval (list or tuple): The interval for the ROPE.
-            group (str, optional): The group to make the decision for ("A", "B", or "diff"). Defaults to "diff".
-            level (int, optional): The confidence level for the HDI. Defaults to 95.
+            rvs (_type_): _description_
+            rope_interval (_type_): _description_
+            level (int, optional): _description_. Defaults to 95.
         """
-        raise NotImplementedError
+        return bayesian_functions.rope_decision(rvs=self.make_rvs_diff(), rope_interval=rope_interval, level=level)
 
-    def map_from_rvs(self, method='kde', bins=30):
+    def map(self, method='median'):
         """
         Estimates the Maximum A Posteriori (MAP) value from posterior samples.
 
         Args:
-            method (str, optional): Method to estimate the MAP ('kde' or 'hist'). Defaults to 'kde'.
-            bins (int, optional): Number of bins to use if method='hist'. Defaults to 30.
+            rvs (array-like): Samples from the posterior distribution.
+            method (str): Method to estimate the MAP ('kde' or 'median').
 
         Returns:
             float: MAP estimate based on the provided samples.
         """
         rvs = self.make_rvs_diff()
-
-        if method == 'kde':
-            # Kernel Density Estimation (KDE)
-            kde = gaussian_kde(rvs)
-            x_vals = np.linspace(np.min(rvs), np.max(rvs), 1000)
-            densities = kde(x_vals)
-            map_estimate = x_vals[np.argmax(densities)]
-        elif method == 'hist':
-            # Histogram-based estimation
-            hist, edges = np.histogram(rvs, bins=bins, density=True)
-            bin_centers = (edges[:-1] + edges[1:]) / 2
-            map_estimate = bin_centers[np.argmax(hist)]
-        else:
-            raise ValueError("Invalid method. Choose 'kde' or 'hist'.")
         
-        return map_estimate
+        return bayesian_functions.map(rvs, method=method)
 
     def bayesian_factor(self, H1=None, H0=None, prior=None, scale_factor=0.1):
-        """
-        Calculates the Bayes factor for the specified hypotheses.
-
-        Args:
-            H1 (list or tuple, optional): The interval for the alternative hypothesis. Defaults to None.
-            H0 (list or tuple, optional): The interval for the null hypothesis. Defaults to None.
-            prior (list or tuple, optional): The prior distribution. Defaults to None.
-            scale_factor (float, optional): The scale factor for the null hypothesis interval. Defaults to 0.1.
-
-        Returns:
-            str: A summary of the Bayes factor and the evidence it provides.
-        """
+        
         rvs = self.make_rvs_diff()
 
         if H0 is None:
@@ -172,20 +118,20 @@ class BayesianModel:
         else:
             if not isinstance(H0, (list, np.array, tuple)):
                 raise ValueError("Null hypothesis must be a interval in values, array or list of length two")
-        p_H0 = 1 - np.mean((rvs < np.max(H0)) & (rvs > np.min(H0)))
+        p_H0 = 1-(np.mean((rvs<np.max(H0)) & (rvs>np.min(H0))))
         H0 = f"Parameter between {np.min(H0):.2f} and {np.max(H0):.2f}"
 
         if H1 is None:
-            H1 = "Parameter larger than 0"
-            p_H1 = np.mean(rvs > 0)
+            H1 = "Parameter larger than 0 or smaller than 0"
+            p_H1 = max(np.mean(rvs>0), np.mean(rvs<0))
         else:
             if not isinstance(H1, (list, np.ndarray, tuple)) and len(H1) == 2:
                 raise ValueError("Alternative hypothesis must be a interval in values, array or list of length two")
             else:
                 H1 = f"Parameter between {np.min(H1):.2f} and {np.max(H1):.2f}"
-                p_H1 = 1 - np.mean((rvs < np.max(H1)) & (rvs > np.min(H1)))
+                p_H1 = 1-(np.mean((rvs<np.max(H1)) & (rvs>np.min(H1))))
 
-        BF = p_H1 / p_H0
+        BF = p_H1/p_H0
 
         # calculate bayes factor given H0 and H1
         # return plain text 
@@ -208,4 +154,23 @@ class BayesianModel:
                 For the alternative hypothesis: {H1}
                 The Bayes factor is {BF:.2f}, thus providing {text}
                 """
+    
+    def summary_result(self, rope_interval, level):
+
+        result = ""
+        result += f"Probablity that A is better than B = {self.prob_best():.2f}% \n\n"
+
+        hdi = self.hdi(level=level)
+        result += f"There is 95% that the difference in Bernoulli probability is between {hdi[0]:.2f} and {hdi[1]:.2f} \n\n"
+
+        result += f"The MAP (maximum a posterior estimate) if {self.map():.2f} \n\n"
+
+        result += f"Probability that the difference is within the ROPE (region of practical equivalence) is {self.rope(interval=rope_interval):.1f}% \n\n"
+
+        result += f"ROPE-based decision: {self.rope_decision(rope_interval, level=level)}  \n\n"
+
+        result += "Bayes factor (A vs B vs null): \n"
+        result += self.bayesian_factor() + "\n\n"
+
+        return result
 
