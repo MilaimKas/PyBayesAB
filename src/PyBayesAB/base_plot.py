eb3d0ca2f838bb429b18a7b5b8f7c71849190131
MilaimKas <milaim.kas@gmail.com>
2025-06-29 15:36:29 +0200

    add post_kwargs

--- src/PyBayesAB/base_plot.py
+++ src/PyBayesAB/base_plot.py
@@ -8,7 +8,7 @@ class PlotManager:
     
     #def __init__(self):
 
-    def plot_final_posterior(self, group, N_sample=N_SAMPLE, N_pts=N_PTS, para_range=None, **kwargs):
+    def plot_final_posterior(self, group, N_sample=N_SAMPLE, N_pts=N_PTS, para_range=None, plot_kwargs={}, post_kwargs={}):
         """
         plot the posterior distribution for the total result
 
@@ -17,48 +17,47 @@ class PlotManager:
             prange (list, optional): [lower, upper] limit for p. Defaults to None.
         """       
 
-
         parameter_name = self.get_parameter_name()
 
         # plot either group A or group B
         if (group == "A") or (group == "B"):
-            rvs = self.make_rvs(group=group, N_sample=N_sample)
+            rvs = self.make_rvs(group=group, N_sample=N_sample, **post_kwargs)
             para_range = para_range or self.get_parameter_range(rvs)
             x_pdf = np.linspace(para_range[0], para_range[1], N_pts)
-            y_pdf = self.make_pdf(group=group, p_pts=x_pdf)
+            y_pdf = self.make_pdf(group=group, p_pts=x_pdf, **post_kwargs)
             fig = plot_functions.plot_posterior([rvs], [x_pdf, [y_pdf]], labels=[group], xlabel=parameter_name, 
-                                                **kwargs)
+                                                **plot_kwargs)
 
         # plot difference of posteriors
         elif group == "diff":
-            rvs_A = self.make_rvs(group="A", N_sample=N_sample) 
-            rvs_B = self.make_rvs(group="B", N_sample=N_sample)
+            rvs_A = self.make_rvs(group="A", N_sample=N_sample, **post_kwargs) 
+            rvs_B = self.make_rvs(group="B", N_sample=N_sample, **post_kwargs)
             rvs_diff = rvs_A-rvs_B
             fig = plot_functions.plot_posterior([rvs_diff], labels=["A-B"], xlabel="Difference in "+parameter_name, 
-                                                **kwargs)
+                                                **plot_kwargs)
 
         # plot both posterior of group A and group B
         elif group == "AB":
-            rvs_A = self.make_rvs(group="A", N_sample=N_sample)
-            rvs_B = self.make_rvs(group="B", N_sample=N_sample)
+            rvs_A = self.make_rvs(group="A", N_sample=N_sample,**post_kwargs)
+            rvs_B = self.make_rvs(group="B", N_sample=N_sample,  **post_kwargs)
             para_range = para_range or self.get_parameter_range(np.concatenate((rvs_A, rvs_B)))
             x_pdf = np.linspace(para_range[0], para_range[1], N_pts)
-            pdf_A = self.make_pdf(group="A", p_pts=x_pdf)
-            pdf_B = self.make_pdf(group="B", p_pts=x_pdf)
+            pdf_A = self.make_pdf(group="A", p_pts=x_pdf, **post_kwargs)
+            pdf_B = self.make_pdf(group="B", p_pts=x_pdf, **post_kwargs)
             fig = plot_functions.plot_posterior([rvs_A, rvs_B], [x_pdf, [pdf_A, pdf_B]], labels=["A", "B"], xlabel=parameter_name, 
-                                                **kwargs)
+                                                **plot_kwargs)
 
         else:
             raise SyntaxError("group can only be A, B, diff or AB")
 
         return fig
     
-    def plot_cum_posterior(self, group, type, N_sample=N_SAMPLE, para_range=None, N_pts=N_PTS):
+    def plot_cum_posterior(self, group, type, N_sample=N_SAMPLE, para_range=None, N_pts=N_PTS, **post_kwargs):
 
         parameter_name = self.get_parameter_name()
 
         # get list of appropriate posteriors
-        rvs_data, pdf_data = self.get_post_data(group=group, para_range=para_range, N_sample=N_sample, N_pts=N_pts)
+        rvs_data, pdf_data = self.get_post_data(group=group, para_range=para_range, N_sample=N_sample, N_pts=N_pts, **post_kwargs)
 
         # plot posterior for one group
         if (group == "A") or (group == "B") or (group == "AB"):
@@ -96,17 +95,17 @@ class PlotManager:
 
         return fig
 
-    def plot_anim(self, group, N_sample=N_SAMPLE, para_range=None, N_pts=N_PTS, interval=200, figsize=FIGSIZE):
+    def plot_anim(self, group, N_sample=N_SAMPLE, para_range=None, N_pts=N_PTS, interval=200, figsize=FIGSIZE, **post_kwargs):
 
         parameter_name = self.get_parameter_name()
 
-        rvs_data, pdf_data = self.get_post_data(group=group, para_range=para_range, N_sample=N_sample, N_pts=N_pts)
+        rvs_data, pdf_data = self.get_post_data(group=group, para_range=para_range, N_sample=N_sample, N_pts=N_pts, **post_kwargs)
         if pdf_data is None:
             pdf_data = rvs_data
         return plot_functions.animate_posterior(pdf_data, interval=interval, figsize=figsize, xlabel=parameter_name)
 
 
-    def get_post_data(self, group, para_range=None, N_sample=N_SAMPLE, N_pts=N_PTS):
+    def get_post_data(self, group, para_range=None, N_sample=N_SAMPLE, N_pts=N_PTS, **post_kwargs):
         """
         post process the posterior data for the given group to be used in plotting
 
@@ -122,21 +121,21 @@ class PlotManager:
 
         if (group == "A") or (group == "B"):
             
-            param_pts, rvs_data, pdf_data = self.make_cum_posterior(group=group, N_sample=N_sample, para_range=para_range, N_pts=N_pts)
+            param_pts, rvs_data, pdf_data = self.make_cum_posterior(group=group, N_sample=N_sample, para_range=para_range, N_pts=N_pts, **post_kwargs)
             rvs_data = [[r] for r in rvs_data]
             pdf_data = [[param_pts], [[p] for p in pdf_data]]
 
         elif group == "AB":
-            param_pts_A , rvs_data_A, pdf_data_A = self.make_cum_posterior(group="A", N_sample=N_sample, para_range=para_range, N_pts=N_pts)
-            param_pts_B, rvs_data_B, pdf_data_B = self.make_cum_posterior(group="B", N_sample=N_sample, para_range=para_range, N_pts=N_pts)
+            param_pts_A , rvs_data_A, pdf_data_A = self.make_cum_posterior(group="A", N_sample=N_sample, para_range=para_range, N_pts=N_pts, **post_kwargs)
+            param_pts_B, rvs_data_B, pdf_data_B = self.make_cum_posterior(group="B", N_sample=N_sample, para_range=para_range, N_pts=N_pts, **post_kwargs)
 
             rvs_data = list(zip(rvs_data_A, rvs_data_B))
             pdf_data = [[param_pts_A, param_pts_B], list(zip(pdf_data_A, pdf_data_B))]
 
         elif group == "diff":
 
-            _, rvs_data_A, _ = self.make_cum_posterior(group="A")
-            _, rvs_data_B, _ = self.make_cum_posterior(group="B")
+            _, rvs_data_A, _ = self.make_cum_posterior(group="A", **post_kwargs)
+            _, rvs_data_B, _ = self.make_cum_posterior(group="B", **post_kwargs)
 
             rvs_data = []
             for rvs_a,rvs_b in zip(rvs_data_A, rvs_data_B):
@@ -159,7 +158,7 @@ class PlotManager:
             parameter_name = "Parameters"
         return parameter_name
 
-    def plot_bayesian_metrics(self, rope_interval, N_sample=N_SAMPLE, N_pts=N_PTS):
+    def plot_bayesian_metrics(self, rope_interval, N_sample=N_SAMPLE, N_pts=N_PTS, **post_kwargs):
         """
         Plots Bayesian metrics (HDI, ROPE, MAP) against the number of experiments using Matplotlib.
 
@@ -181,7 +180,7 @@ class PlotManager:
         num_experiments = list(range(1, len(self.return_data("A")) + 1))
 
         # get posteriors
-        rvs_data, _ = self.get_post_data(group="diff", N_sample=N_sample, para_range=None, N_pts=N_pts)
+        rvs_data, _ = self.get_post_data(group="diff", N_sample=N_sample, para_range=None, N_pts=N_pts, **post_kwargs)
         
         # Calculate metrics for each number of experiments
         for rvs in rvs_data:
@@ -209,3 +208,25 @@ class PlotManager:
             )
 
         return fig1,  fig2
+    
+    def plot_data(self, group="A", **plot_kwargs):
+        """
+        Plot the data for a given group.
+
+        Args:
+            group (str, optional): The group to plot data for ("A" or "B"). Defaults to "A".
+
+        Returns:
+            matplotlib.figure.Figure: The Matplotlib figure with the data plot.
+        """
+        if group not in ["A", "B"]:
+            raise ValueError("Group must be either 'A' or 'B'.")
+
+        data = self.return_data(group)
+
+        if "label" not in plot_kwargs:
+            plot_kwargs["label"] = group
+            
+        fig = plot_functions.plot_data(data, **plot_kwargs)
+
+        return fig
